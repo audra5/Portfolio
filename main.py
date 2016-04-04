d2f69080f0c7531925af1436a716d37dc9804d99
audra5 <achr@umich.edu>
2016-04-04 03:37:11 -0400

    added email confirmation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import webapp2
 import os
 import logging
 import jinja2
+from google.appengine.api import mail
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -11,28 +12,66 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
+        name = self.request.path
         template = JINJA_ENVIRONMENT.get_template('templates/index.html')
         self.response.write(template.render())
 
 class Page1Handler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/Audra.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/about.html')
         self.response.write(template.render())
 
 class Page2Handler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/Amanda.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/gallery.html')
         self.response.write(template.render())
 
 class Page3Handler(webapp2.RequestHandler):
     def get(self):
-        template = JINJA_ENVIRONMENT.get_template('templates/TheirStory.html')
+        template = JINJA_ENVIRONMENT.get_template('templates/faq.html')
         self.response.write(template.render())
+
+class Page4Handler(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
+        self.response.write(template.render({"msg": "Sign-up for e-mail updates"}))
+    def post(self):
+
+        email1 = self.request.get("email1")
+        email2 = self.request.get("email2")
+
+        if email1 == email2:
+            template = JINJA_ENVIRONMENT.get_template('templates/success.html')
+            self.response.write(template.render())
+
+            message = mail.EmailMessage(sender="achr@umich.edu",
+                                subject="Audranda")
+
+            message.to = email1
+            message.body = """
+            Hello! 
+
+            Thank you for signing up with Audranda! Please, enjoy this goofy picture of Audra and Amanda face-swapping. 
+
+
+            Yours truly,
+            Audranda
+
+            """
+
+            message.send()
+        else:
+            template = JINJA_ENVIRONMENT.get_template('templates/contact.html')
+            self.response.write(template.render({"msg": "Your e-mail addresses did not match - try again"}))
+
+           
+
 
 app = webapp2.WSGIApplication([
     ('/', IndexHandler),
     ('/index.html', IndexHandler),
-    ('/Audra.html', Page1Handler),
-    ('/Amanda.html', Page2Handler),
-    ('/TheirStory.html', Page3Handler)
+    ('/about.html', Page1Handler),
+    ('/gallery.html', Page2Handler),
+    ('/faq.html', Page3Handler),
+    ('/contact.html', Page4Handler)
 ], debug=True)
